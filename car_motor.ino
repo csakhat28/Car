c878aeed14893ffeb3ba3a165537c2e4eb97581e
Thomas Gallagher <tgallagher@aapl000617.local>
2018-02-20 09:16:02 -0500

    programmed motor

---
+++ car_motor.ino
@@ -0,0 +1,23 @@
+void setup()
+{
+  pinMode(12, INPUT);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+}
+
+void loop(){
+
+int button = digitalRead(12);
+
+  if (2>1){
+
+  digitalWrite(13,HIGH );
+  
+}
+  else{ 
+    digitalWrite(13,LOW);
+  
+  
+  } 
+}
+  
